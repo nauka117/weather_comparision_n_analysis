3ba62d1a4adb626b89f6c955ebdfbc721dc2f10a
nauka117 <v_kozyrev@outlook.com>
2025-01-16 19:00:47 +0300

    feat: rewrote getting parameters mechanism
    now i use pydantic instead dotenv. that allows to validate data

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,11 +1,26 @@
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-OWM_api_key = os.getenv('OWM_api_key')
-GCuseragent = os.getenv('user_agent')
+from pydantic_settings import BaseSettings
+from pydantic import field_validator, ValidationError
+import re
 
 
-print(f"openweathermap API Key: {OWM_api_key}")
-print(f"geocoder user agent: {GCuseragent}")
+class Settings(BaseSettings):
+    OWM_api_key: str
+    GC_user_agent: str
+
+    class Config:
+        env_file = ".env"
+    
+    @field_validator("OWM_api_key")
+    def validate_OWM_api_key(cls, value):
+        if len(value) != 32:
+            raise ValueError("OWM_api_key must be 32 characters long.")
+        if not re.fullmatch(r"[a-f0-9]{32}", value):
+            raise ValueError("OWM_api_key must be a 32-character hexadecimal string.")
+        return value
+
+try:
+    settings = Settings()
+    print(f"openweathermap API Key: {settings.OWM_api_key}")
+    print(f"geocoder user agent: {settings.GC_user_agent}")
+except ValidationError as e:
+    print(".env file configuration error:", e)
