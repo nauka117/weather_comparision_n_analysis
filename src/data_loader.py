338e49a026e38a350c095263a7e97be732b15ff1
nauka117 <v_kozyrev@outlook.com>
2025-01-17 22:22:52 +0300

    build: now i use poetry instead of pip test: initialized test folder, data_loader testing file

--- src/data_loader.py
+++ src/data_loader.py
@@ -2,6 +2,9 @@ from pydantic_settings import BaseSettings
 from pydantic import field_validator, ValidationError
 import re
 
+import datetime as dt
+
+import requests
 
 class Settings(BaseSettings):
     OWM_api_key: str
@@ -24,3 +27,68 @@ try:
     print(f"geocoder user agent: {settings.GC_user_agent}")
 except ValidationError as e:
     print(".env file configuration error:", e)
+
+
+class HistoricalWeatherRequest:
+    base_url: str
+    api_key: str
+
+    location: list[float]
+    start_date: dt.datetime
+    end_date: dt.datetime
+
+    def setLocationCoordinates(self, longitude: float, latitude: float):
+        self.location = [longitude, latitude]
+    
+    def setLocationCity(self, city_name):
+        base_url = "https://nominatim.openstreetmap.org/search"
+        params = {
+            "q": city_name,
+            "format": "json",
+            "addressdetails": 0,
+            "limit": 1  # limit for results. We need only 1st (most relevant) result
+        }
+        headers = {
+            "User-Agent": settings.GC_user_agent  # required by Nominatim API to identify the request source
+        }
+        response = requests.get(base_url, params=params, headers=headers)
+        if response.status_code == 200:
+            results = response.json()
+            if results:
+                self.location = [results[0]["lat"], results[0]["lon"]]
+            else:
+                print(f"No results found for city: {city_name}")
+                return None
+        else:
+            print(f"Error: {response.status_code}")
+            return None
+    
+    def setDate(self, date: str):
+        
+        formats = [
+            "%Y-%m-%d",  # ISO 8601 format  -   YYYY-MM-DD
+            "%d.%m.%Y",  # Russian format   -   DD.MM.YYYY
+            "%m.%d.%Y",  # US format        -   MM/DD/YYYY
+            "%d/%m/%Y",  # European format  -   DD/MM/YYYY
+        ]
+
+        for fmt in formats:
+            try:
+                date_obj = dt.datetime.strptime(date, fmt)
+                unix_timestamp = int(date_obj.timestamp())
+                self.start_date = unix_timestamp
+            except ValueError:
+                continue
+
+        raise ValueError(f"Date '{date}' is not in a recognized format. \nAllowed formats: YYYY-MM-DD, DD.MM.YYYY, MM/DD/YYYY, DD/MM/YYYY")
+
+    
+
+class HistoricalWeatherRequest_OWM(HistoricalWeatherRequest):
+    """Subclass of HistoricalWeatherRequest for OpenWeatherMap API"""
+    def __init__(self):
+        self.base_url = "https://history.openweathermap.org/data/2.5/history/city"
+        self.api_key = settings.OWM_api_key
+
+
+summa = lambda a, b: a + b
---
+++ tests/test_data_loader.py
@@ -0,0 +1,6 @@
+from src.data_loader import (
+    summa
+)
+
+def test_summa():
+    assert summa(1, 2) == 3
